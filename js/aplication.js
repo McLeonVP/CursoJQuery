67a793fd0eaec8501a21c30a6ae9ba559e0b82c4
LeonardoRojas <leoluisrojasramirez@gmail.com>
2020-02-26 21:05:14 -0300

    Mi Primer Commit

---
+++ js/aplication.js
@@ -0,0 +1,13 @@
+
+/*
+let texto=$("item_1").text();
+$("h3").text(texto);
+
+console.log(texto)
+*/
+
+$(".hot").click(function(){
+	//console.log("Funciona");
+	let texto=$(this).text();
+$("h3").text(texto)
+});
